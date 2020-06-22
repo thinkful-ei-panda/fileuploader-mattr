7367413636b500408d0551a8f0e98c4ab6c78741
Matthew Rougely <matthewrougelysr@gmail.com>
2020-06-22 10:00:05 -0700

    Completed Event Handlers and CSS

---
+++ src/SearchBar/SearchBar.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './SearchBar.css';
+import SearchBox from '../SearchBox/SearchBox';
+import FilterOptions from '../FilterOptions/FilterOptions';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <div className="SearchBar">
+        <div className="SearchBar__heading">
+          <h1>File Uploader</h1>
+        </div>
+        <div className="SearchBar__controls">
+          <SearchBox
+            searchTerm={this.props.searchTerm}
+            handleUpdate={this.props.handleUpdate}/>
+          <FilterOptions
+            filterOption={this.props.filterOption}
+            handleFilterChange={this.props.handleFilterChange}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
---
+++ src/SearchBox/SearchBox.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './SearchBox.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+class SearchBox extends Component {
+  render() {
+    return (
+      <div className="SearchBox">
+        <FontAwesomeIcon icon={faSearch}/>
+        <input
+          placeholder="Search term"
+          value={this.props.searchTerm}
+          onChange={e => this.props.handleUpdate(e.target.value)}/>
+      </div>   
+    );
+  }
+}
+
+export default SearchBox;
